95abbec6aec0174c33dce47bb0280a08f0018f6a
felipe <enggazolla@gmail.com>
2024-09-15 10:55:39 -0300

    light statusbar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { StatusBar } from "react-native"
 import Home from "./src/screens/Home/indes"
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar barStyle={'light-content'} />
+      <Home />
+    </>
   )
 }
