6a476505f6fbef164ea8de8da1b5e7e5acc5018b
felipe <enggazolla@gmail.com>
2024-09-14 22:29:24 -0300

    home screen

--- src/screens/Home/indes.tsx
+++ src/screens/Home/indes.tsx
@@ -1,5 +1,6 @@
-import { Image, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
+import { Clipboard, PlusCircle } from 'lucide-react-native'
 
 export default function Home() {
   return (
@@ -7,15 +8,55 @@ export default function Home() {
 
       <Image source={require('../../../assets/logo.png')} alt='todo logo' />
 
-      <View style={styles.inputContainer}>
-      <TextInput style={styles.input}/>
+      <View style={styles.form}>
+      <TextInput style={styles.input} placeholder='Adicione uma nova tarefa' placeholderTextColor="#808080"/>
       <TouchableOpacity style={styles.button}>
-        
+        <PlusCircle color='#FFF' size={20} />
       </TouchableOpacity>
       </View>
 
-      <View>
+      <View style={styles.backgroundTasks} >
+        <View style={styles.tasks}>
 
+          <View style={styles.countertasks}>
+            <View style={styles.desirecounters}>
+          <Text style={styles.created}>
+            Criadas
+          </Text>
+          <View style={styles.backgroudnCounter}>
+            <Text style={styles.counter}>
+              0
+            </Text>
+            </View>
+            </View>
+          <View style={styles.desirecounters}>
+          <Text style={styles.done}>
+          Concluídas
+          </Text>
+            <View style={styles.backgroudnCounter}>
+            <Text style={styles.counter}>
+              0
+            </Text>
+            </View>
+            </View>
+          </View>
+
+        <View style={styles.separator} />
+
+
+        <View style={styles.empty}>
+          <Clipboard size={80} color='#333333'/>
+          <Text style={styles.textemptybold}>
+          Você ainda não tem tarefas cadastradas
+          </Text>
+          <Text style={styles.textempty}>
+          Crie tarefas e organize seus itens a fazer
+          </Text>
+
+        </View>
+
+
+        </View>
       </View>
     </View>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,20 +6,100 @@ export const styles = StyleSheet.create({
       backgroundColor: '#0D0D0D',
       paddingTop: 80,
       alignItems: 'center',
+      fontFamily: 'Inter'
     },
-  inputContainer: {
+  form: {
+    marginTop: 40,
     width: 327,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
   },
   input: {
-      marginTop: 40,
       width: 271,
       height: 54,
       backgroundColor: '#262626',
       borderRadius: 6,
+      padding: 16,
+      color: '#FFFFFF',
+      fontSize: 16,
   },
   button: {
-    color: '#1E6F9F',
+    backgroundColor: '#1E6F9F',
+    color: '#FFFFFF',
     width: 52,
     height: 54,
-  }
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6
+  },
+  backgroundTasks: {
+    backgroundColor: '#1A1A1A',
+    width: '100%',
+    marginTop: 190,
+    height: '100%',
+    position: 'absolute',
+    zIndex: -1
+  },
+  tasks: {
+    marginTop: 56,
+    marginLeft: 24,
+    marginRight: 24,
+  },
+  created: {
+    color: '#4EA8DE',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  counter: {
+    flexDirection: 'row',
+    color: '#D9D9D9',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  done: {
+    color: '#8284FA',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  countertasks: {
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  backgroudnCounter: {
+    backgroundColor: '#333333',
+    borderRadius: 50,
+    width: 25,
+    height: 19,
+  },
+  desirecounters: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center'
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#333333',
+    marginTop: 40,
+  },
+  empty: {
+    alignItems: 'center',
+    marginTop: 56,
+  },
+  textemptybold: {
+    color: '#808080',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 40,
+  },
+  textempty: {
+    color: '#808080',
+    fontSize: 16,
+
+  },
 })
